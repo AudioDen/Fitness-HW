087c2be9d5d6f21ab047172fc443569093d597d1
AudioDen <chicagosoundman@gmail.com>
2021-06-05 16:53:25 -0500

    duration working

--- models/workout.js
+++ models/workout.js
@@ -45,7 +45,13 @@ const WorkoutSchema = new Schema(
     ],
    
   },
+  { toJSON: { virtuals: true } },
+  { toObject: { virtuals: true } }
 );
-
+WorkoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+})
 const Workout = mongoose.model("workout", WorkoutSchema);
 module.exports = Workout;
